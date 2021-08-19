4fb105649f05c28052326cfe03e9156f3fc06dca
TonTon69 <hoanghuynh0609@gmail.com>
2021-08-19 16:18:41 +0700

    update scroll to top

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,6 +1,10 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
+const bgLight = $(".bg-light");
+const navLink = $(".nav-link");
+console.log(navLink);
+
 window.onscroll = function () {
     if (this.scrollY > 20) {
         document.body.classList.add("show");
@@ -10,6 +14,12 @@ window.onscroll = function () {
 };
 
 const toTop = $(".to-top span");
-toTop.onclick = function () {
-    window.scrollTo({ top: 0, behavior: "smooth" });
+toTop.onclick = function scrollToTop() {
+    if (
+        document.body.scrollTop !== 0 ||
+        document.documentElement.scrollTop !== 0
+    ) {
+        window.scrollBy(0, -100);
+        requestAnimationFrame(scrollToTop);
+    }
 };
