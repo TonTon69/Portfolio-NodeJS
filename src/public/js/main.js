400f9a2f69604ca80eecfbc68b64d54867dd94bb
TonTon69 <hoanghuynh0609@gmail.com>
2021-08-20 15:54:20 +0700

    update CRUD project

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -19,23 +19,3 @@ toTop.onclick = function scrollToTop() {
         requestAnimationFrame(scrollToTop);
     }
 };
-
-var deleteProjectModal = $("#delete-project-modal");
-var projectId;
-var deleteProjectForm = document.forms["delete-project-form"];
-
-deleteProjectModal.addEventListener("show.bs.modal", function (event) {
-    // Button that triggered the modal
-    var button = event.relatedTarget;
-    projectId = button.getAttribute("data-bs-id");
-});
-
-var btnDeleteProject = $("#btn-delete-project");
-//When delete project btn clicked
-btnDeleteProject.onclick = function () {
-    deleteProjectForm.setAttribute(
-        "action",
-        "/projects/" + projectId + "?_method=DELETE"
-    );
-    deleteProjectForm.submit();
-};
